34eab0e53f01b31cf02d6091c460362436dd0195
Tom Doder <tdoder89@gmail.com>
2023-03-06 15:37:53 -0600

    add catches to fetch functionality

---
+++ test/data-test.js
@@ -0,0 +1,6 @@
+import { expect } from 'chai';
+import Booking from '../src/classes/Booking';
+import Customer from '../src/classes/customers';
+import Rooms from '../src/classes/rooms';
+import Data from '../src/classes/Data';
+import testData from '../src/data/test-data';
